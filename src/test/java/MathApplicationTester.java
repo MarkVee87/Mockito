0acce0d65e83de10bc97168df167d7d051d912cf
Mark Vallis <1034528@my.brunel.ac.uk>
2017-05-03 20:14:47 +0100

    Update MathApplicationTester.java

--- src/test/java/MathApplicationTester.java
+++ src/test/java/MathApplicationTester.java
@@ -18,36 +18,56 @@ public class MathApplicationTester {
     private CalculatorService calculatorService;
 
     @Test
-    public void testAdd(){
+    public void testAddBasic(){
         when(calculatorService.add(10.0, 20.0)).thenReturn(30.0);
         Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
-
-        // check that the required arguments were provided to the add method
-//        verify(calculatorService).add(10.0, 20.0);
-
-        // check that the add method was called only once, no more, no less (add the additional assertion statement below to test)
-//        Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
-//        verify(calculatorService, times(1)).add(anyDouble(), anyDouble());
-
-        // check that the multiplication method was never called
-        verify(calculatorService, never()).multiply(anyDouble(), anyDouble());
-
-        // check that the add subtract method was called at least once
-        when(calculatorService.subtract(anyDouble(), anyDouble())).thenReturn(40.0);
-        Assert.assertEquals(mathApplication.subtract(50.0, 10.0), 40.0, 0);
-        verify(calculatorService, atLeastOnce()).subtract(anyDouble(), anyDouble());
-
-        // check that the division method was called at least or at most 3 times
-        when(calculatorService.divide(anyDouble(), anyDouble())).thenReturn(2.0);
-        Assert.assertEquals(mathApplication.divide(10.0, 5.0), 2.0, 0);
-//        verify(calculatorService, atLeast(3)).divide(anyDouble(), anyDouble());
-        verify(calculatorService, atMost(3)).divide(anyDouble(), anyDouble());
     }
 
     @Test
     public void testAddAny(){
         when(calculatorService.add(anyDouble(), anyDouble())).thenReturn(30.0);
         Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+    }
+
+    @Test
+    public void testAddArgs(){
+        // check that the required arguments were provided to the add method
+        when(calculatorService.add(anyDouble(), anyDouble())).thenReturn(30.0);
+        Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+        verify(calculatorService).add(10.0, 20.0);
+    }
+
+    @Test
+    public void testAddCalledOnce(){
+        // check that the add method was called only once, no more, no less (add the additional assertion statement below to test)
+        when(calculatorService.add(anyDouble(), anyDouble())).thenReturn(30.0);
+        Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+        verify(calculatorService, times(1)).add(anyDouble(), anyDouble());
+    }
+
+    @Test
+    public void testAddCalledAtLeastOnce(){
+        // check that the add subtract method was called at least once
+        when(calculatorService.subtract(anyDouble(), anyDouble())).thenReturn(40.0);
+        Assert.assertEquals(mathApplication.subtract(50.0, 10.0), 40.0, 0);
+        verify(calculatorService, atLeastOnce()).subtract(anyDouble(), anyDouble());
+    }
+
+    @Test
+    public void testMultNeverCalled(){
+        // check that the multiplication method was never called
+        when(calculatorService.add(anyDouble(), anyDouble())).thenReturn(30.0);
+        Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+        verify(calculatorService, never()).multiply(anyDouble(), anyDouble());
+    }
+
+    @Test
+    public void testDivCalledMinMax(){
+        // check that the division method was called at least or at most 3 times
+        when(calculatorService.divide(anyDouble(), anyDouble())).thenReturn(2.0);
+        Assert.assertEquals(mathApplication.divide(10.0, 5.0), 2.0, 0);
+//        verify(calculatorService, atLeast(3)).divide(anyDouble(), anyDouble());
+        verify(calculatorService, atMost(3)).divide(anyDouble(), anyDouble());
     }
 
     @Test
